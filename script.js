d8ecb4c1238e8a1a5878a9dbc21ea95087e167ab
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-04 11:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function calculateGrade(percentage) {
+function calculateGrade(p) {
   //your code here
+	if(p>=90) return 'A';
+	else if(p>=80 && p<90) return 'B';
+	else if(p>=70 && p<80) return 'C';
+	else if(p>=60 && p<70) return 'D';
+	else if(p<60) return 'F';
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
